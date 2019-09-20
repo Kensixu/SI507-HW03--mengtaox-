e15af94c0c19a7345da64d09bfd404e484e55dd5
ziyizh <nubszzy@163.com>
2019-09-20 14:33:25 -0400

    add random choice in branch

--- magic_eight.py
+++ magic_eight.py
@@ -8,4 +8,3 @@ magic_answer = ["It is certain.", "It is decidedly so.", "Without a doubt", "Yes
  "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
  "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
 answer = random.choice(magic_answer)
-print(answer)
